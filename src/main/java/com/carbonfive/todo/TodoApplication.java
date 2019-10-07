a771bdd5ab16bdf58616a02a21f8482e28e0411f
Will Ockelmann-Wagner <wow@carbonfive.com>
2019-10-07 13:39:04 -0700

    i18n

--- src/main/java/com/carbonfive/todo/TodoApplication.java
+++ src/main/java/com/carbonfive/todo/TodoApplication.java
@@ -1,13 +1,24 @@
 package com.carbonfive.todo;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
 @SpringBootApplication
 public class TodoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TodoApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(TodoApplication.class, args);
+  }
 
+  @Bean
+  public LocaleResolver localeResolver() {
+    AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+    localeResolver.setDefaultLocale(Locale.US);
+    return localeResolver;
+  }
 }
--- src/main/java/com/carbonfive/todo/user/UserController.java
+++ src/main/java/com/carbonfive/todo/user/UserController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +20,9 @@ import org.springframework.web.server.ResponseStatusException;
 public class UserController {
   @Autowired
   private UserDaoService service;
+
+  @Autowired
+  private MessageSource messageSource; 
 
   @GetMapping("/users")
   public List<User> getUsers() {
@@ -40,5 +45,10 @@ public class UserController {
   public User createUser(@Valid @RequestBody User user) {
     service.save(user);
     return user;
+  }
+
+  @GetMapping(path = "/hello-world-internationalized")
+  public String helloWorldInternationalized() {
+    return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
   }
 }
